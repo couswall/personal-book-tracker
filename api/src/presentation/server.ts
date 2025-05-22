e31cc7540d56daa5a9fce36b169c10b116749f59
Andre Ignorosa <andreiglu@hotmail.com>
2025-05-22 12:30:41 -0600

    config: install express and set public path

--- api/src/presentation/server.ts
+++ api/src/presentation/server.ts
@@ -1,21 +1,31 @@
+import express from "express";
 import { Server as HttpServer } from 'http';
 
 interface ServerOptions{
     port: number;
+    publicPath: string;
 }
 
 export class Server{
+    public readonly app = express();
     private readonly port: number;
     private serverListener?: HttpServer;
+    private readonly publicPath: string;
 
     constructor(options: ServerOptions){
-        const {port} = options;
+        const {port, publicPath} = options;
         
         this.port = port;
+        this.publicPath = publicPath;
     };
 
     async start(){
-        console.log(`Server running in port ${this.port}`);
+        
+        this.app.use(express.static(this.publicPath));
+
+        this.serverListener = this.app.listen(this.port, () => {
+            console.log(`Server running in port ${this.port}`);
+        });
     }
 
 
