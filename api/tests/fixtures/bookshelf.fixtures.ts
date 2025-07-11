e6ac33bea415f4c27a143912e10b335b4badb647
Andre Ignorosa <andreiglu@hotmail.com>
2025-07-11 08:06:58 -0600

    api-test: add unit tests for createCustom and getMyBookshelves in bookshelf datasource and repository

---
+++ api/tests/fixtures/bookshelf.fixtures.ts
@@ -0,0 +1,20 @@
+import { BookshelfType } from "generated/prisma";
+import { BookshelfEntity } from "@domain/entities";
+import { userEntity } from "tests/fixtures/user.fixtures";
+
+export const bookshelfObj = {
+    id: 1,
+    name: 'Testing bookshelf',
+    type: BookshelfType.TO_BE_READ,
+    userId: 1,
+    deletedAt: null,
+    books: [],
+    user: userEntity
+};
+
+export const bookshelfEntity = BookshelfEntity.fromObject(bookshelfObj);
+
+export const createCustomBookshelfDto = {
+    userId: bookshelfObj.id,
+    shelfName: 'Custom bookshelf',
+};
