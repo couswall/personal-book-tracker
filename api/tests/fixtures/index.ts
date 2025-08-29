aa671200607b4136b51a83a9bd748e5ce5cc0941
Andre Ignorosa <andreiglu@hotmail.com>
2025-08-29 08:45:02 -0600

    api-test: test updateBookshelfDto

--- api/tests/fixtures/index.ts
+++ api/tests/fixtures/index.ts
@@ -2,3 +2,4 @@ export * from './user.fixtures';
 export * from './bookshelf.fixtures';
 export * from './book.fixtures';
 export * from './apiBooks.fixtures';
+export * from './bookshelfBook.fixtures';
